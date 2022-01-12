d692d3b159f87a9e08389e2daa20c4019ab0bc1d
ElenaLj <elenaljubijankic@gmail.com>
2022-01-12 20:28:12 +0100

    fix contact form layout

--- src/components/sections/Contacts.vue
+++ src/components/sections/Contacts.vue
@@ -8,29 +8,46 @@
               </div>
           </div>
 
-          <div class="row">
-              <div class="col-6">
-                  <img src="../../assets/images/StaticMapService.GetMapIma.png" alt="San Francisco Map">
-              </div>
-              <div class="col-6 contacts__info">
-                  <LogoandLogoText/>
-                  <p class="contacts__address">9840 Shore St. Fond Du Lac, WI 549335</p>
-                  <p class="contacts__phone">0 1223 456 789</p>
-                  <p class="contacts__mail">mail@sofbox.com</p>
+            <div class="row">
+                <div class="col-6">
+                    <img src="../../assets/images/StaticMapService.GetMapIma.png" alt="San Francisco Map">
+                </div>
 
-                  <div class="contacts__icons"></div>
+                <div class="col-6 contacts__info">
+                    <LogoandLogoText/>
+                    <p class="contacts__address">9840 Shore St. Fond Du Lac, WI 549335</p>
+                    <p class="contacts__phone">0 1223 456 789</p>
+                    <p class="contacts__mail">mail@sofbox.com</p>
+                    
+                    <div class="contacts__icons"></div>
 
-                  <div class="contacts__form d-flex">
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="name" placeholder="Your Name">
-                        <label for="name">Your Name</label>
-                    </div>
-                    <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Your Email</label>
-                    </div>
-                  </div>
-              </div>
+                    <!-- Contact form -->
+                    <form>
+                        <div class="row d-flex g-2">
+                            <div class="col-6">
+                                <label for="name"></label>
+                                <input type="text" class="form-control" id="name" placeholder="Your Name">
+                            </div>
+                            <div class="col-6">
+                                <label for="email"></label>
+                                <input type="email" class="form-control" id="email" placeholder="Your Email">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <label for="subject"></label>
+                                <input type="text" class="form-control" id="subject" placeholder="Your Subject">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <label for="message"></label>
+                                <input type="text" class="form-control" id="message" placeholder="Your Message">
+                            </div>
+                        </div>
+                    </form>
+                    <!-- / Contact form -->
+                </div>
           </div>
         </div>
   </section>
@@ -53,6 +70,6 @@ export default {
 
     &__info {
         text-align: left;
-    }
+    } 
 }
 </style>
